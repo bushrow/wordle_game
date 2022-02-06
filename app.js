53e9e2eb5bda343eab29c52d25813381cccb5f7d
Charlie Bushrow <charlie.bushrow@gmail.com>
2022-02-06 02:07:00 -0500

    added keyboard

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+let rows = { row1: "qwertyuiop", row2: "asdfghjkl", row3: "zxcvbnm" };
+
+let brd = document.getElementById("keyboard");
+for (let row in rows) {
+  let letters = rows[row];
+  let keyRow = document.createElement("div");
+  keyRow.classList.add("keyboard-row");
+  for (let i = 0; i < letters.length; i++) {
+    let ele = document.createElement("div");
+    ele.id = letters[i];
+    ele.classList.add("key");
+    ele.textContent = letters[i].toUpperCase();
+    keyRow.appendChild(ele);
+  }
+  brd.appendChild(keyRow);
+}
+
+
+
+
 function getWord() {
-  var theUrl = "https://6qst7bhcdh.execute-api.us-east-1.amazonaws.com/default/select_wordle";
-  var xmlHttp = new XMLHttpRequest();
+  let theUrl = "https://6qst7bhcdh.execute-api.us-east-1.amazonaws.com/default/select_wordle";
+  let xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", theUrl, false); // false for synchronous request
   xmlHttp.send(null);
   return xmlHttp.responseText;
@@ -9,8 +29,8 @@ function getWord() {
 const myWord = getWord()
 
 function nextBox(e, currentBox) {
-  var nextBox = currentBox.nextElementSibling;
-  var prevBox = currentBox.previousElementSibling;
+  let nextBox = currentBox.nextElementSibling;
+  let prevBox = currentBox.previousElementSibling;
 
   if (nextBox == null && e.keyCode === 13) {
     var currentRow = currentBox.parentElement;
@@ -30,16 +50,20 @@ function nextBox(e, currentBox) {
 }
 
 function checkInput(currentRow) {
-  var entry = "";
+  let entry = "";
   for (let i = 0; i < currentRow.children.length; i++) {
-    var letter = currentRow.children[i].value
+    let letter = currentRow.children[i].value
     entry += letter;
+    let letterKey = document.getElementById(letter);
     if (letter === myWord.charAt(i)) {
-      currentRow.children[i].classList.add("correct")
+      currentRow.children[i].classList.add("correct");
+      letterKey.classList.add("correct");
     } else if (myWord.includes(letter)) {
-      currentRow.children[i].classList.add("included")
+      currentRow.children[i].classList.add("included");
+      letterKey.classList.add("included");
     } else {
-      currentRow.children[i].classList.add("incorrect")
+      currentRow.children[i].classList.add("incorrect");
+      letterKey.classList.add("incorrect");
     }
   }
 
@@ -47,7 +71,7 @@ function checkInput(currentRow) {
     alert("CORRECT!")
     return;
   } else {
-    var nextRow = currentRow.nextElementSibling;
+    let nextRow = currentRow.nextElementSibling;
     nextRow.firstElementChild.focus();
     nextRow.firstElementChild.select();
   }
