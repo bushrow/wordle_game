524846732a9520692fd564f3998c59a1f3530662
Charlie Bushrow <charlie.bushrow@gmail.com>
2022-02-06 11:28:57 -0500

    updated keyboard and restricted keystroke entries to letters

--- app.js
+++ app.js
@@ -5,18 +5,33 @@ for (let row in rows) {
   let letters = rows[row];
   let keyRow = document.createElement("div");
   keyRow.classList.add("keyboard-row");
+
+  if (row === "row3") {
+    let ele = document.createElement("div");
+    ele.id = "enterKey";
+    ele.classList.add("key");
+    ele.textContent = "ENTER";
+    keyRow.appendChild(ele);
+  }
+
   for (let i = 0; i < letters.length; i++) {
     let ele = document.createElement("div");
-    ele.id = letters[i];
+    ele.id = letters[i] + "Key";
     ele.classList.add("key");
     ele.textContent = letters[i].toUpperCase();
     keyRow.appendChild(ele);
   }
+
+  if (row === "row3") {
+    let ele = document.createElement("div");
+    ele.id = "delKey";
+    ele.classList.add("key");
+    ele.textContent = "<=";
+    keyRow.appendChild(ele);
+  }
+
   brd.appendChild(keyRow);
 }
-
-
-
 
 function getWord() {
   let theUrl = "https://6qst7bhcdh.execute-api.us-east-1.amazonaws.com/default/select_wordle";
@@ -29,24 +44,27 @@ function getWord() {
 const myWord = getWord()
 
 function nextBox(e, currentBox) {
+  console.log(e.keyCode);
+  if ((e.keyCode !== 13 && e.keyCode !== 8) && (e.keyCode < 65 || e.keyCode > 90)) {
+    currentBox.value = "";
+    return;
+  }
+
   let nextBox = currentBox.nextElementSibling;
   let prevBox = currentBox.previousElementSibling;
 
-  if (nextBox == null && e.keyCode === 13) {
-    var currentRow = currentBox.parentElement;
-    checkInput(currentRow);
+  if (nextBox == null) {
+    if (e.keyCode === 13) {
+      var currentRow = currentBox.parentElement;
+      checkInput(currentRow);
+    }
   } else if (currentBox.value.length >= currentBox.maxLength) {
     nextBox.focus();
     nextBox.select();
-  } else if (currentBox.value.length === 0) {
-    if (prevBox == null) {
-      return;
-    } else {
-      prevBox.focus();
-      prevBox.select();
-    }
+  } else if (currentBox.value.length === 0 && prevBox != null) {
+    prevBox.focus();
+    prevBox.select();
   }
-
 }
 
 function checkInput(currentRow) {
@@ -54,9 +72,10 @@ function checkInput(currentRow) {
   for (let i = 0; i < currentRow.children.length; i++) {
     let letter = currentRow.children[i].value
     entry += letter;
-    let letterKey = document.getElementById(letter);
+    let letterKey = document.getElementById(letter + "Key");
     if (letter === myWord.charAt(i)) {
       currentRow.children[i].classList.add("correct");
+      letterKey.classList.remove("included");
       letterKey.classList.add("correct");
     } else if (myWord.includes(letter)) {
       currentRow.children[i].classList.add("included");
@@ -68,7 +87,6 @@ function checkInput(currentRow) {
   }
 
   if (entry === myWord) {
-    alert("CORRECT!")
     return;
   } else {
     let nextRow = currentRow.nextElementSibling;
