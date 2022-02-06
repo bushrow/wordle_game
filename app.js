0f414655798b14baa8894a8f53cfeb2b4f283509
Charlie Bushrow <charlie.bushrow@gmail.com>
2022-02-05 23:49:49 -0500

    creating wordle-type game

---
+++ app.js
@@ -0,0 +1,46 @@
+const myWord = "above"
+
+function nextBox(e, currentBox) {
+  var nextBox = currentBox.nextElementSibling;
+  var prevBox = currentBox.previousElementSibling;
+
+  if (nextBox == null && e.keyCode === 13) {
+    var currentRow = currentBox.parentElement;
+    checkInput(currentRow);
+  } else if (currentBox.value.length >= currentBox.maxLength) {
+    nextBox.focus();
+    nextBox.select();
+  } else if (currentBox.value.length === 0) {
+    if (prevBox == null) {
+      return;
+    } else {
+      prevBox.focus();
+      prevBox.select();
+    }
+  }
+
+}
+
+function checkInput(currentRow) {
+  var entry = "";
+  for (let i = 0; i < currentRow.children.length; i++) {
+    var letter = currentRow.children[i].value
+    entry += letter;
+    if (letter === myWord.charAt(i)) {
+      currentRow.children[i].classList.add("correct")
+    } else if (myWord.includes(letter)) {
+      currentRow.children[i].classList.add("included")
+    } else {
+      currentRow.children[i].classList.add("incorrect")
+    }
+  }
+
+  if (entry === myWord) {
+    alert("CORRECT!")
+    return;
+  } else {
+    var nextRow = currentRow.nextElementSibling;
+    nextRow.firstElementChild.focus();
+    nextRow.firstElementChild.select();
+  }
+}
